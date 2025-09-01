7057710ef5fad9f2ec89b1299e49df254c42075c
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-09-01 20:53:40 +0530

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
@@ -10,17 +9,14 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <MailchimpForm />
-          <Col size={12} sm={6}>
-            <img src="https://i.ibb.co/xK5XmWh6/Screenshot-2025-09-01-143450-removebg-preview.png" alt="Logo" />
-          </Col>
+          
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="#"><img src={navIcon1} alt="Icon" /></a>
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p> Copyright 2025. © Mohit <br></br>   All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
